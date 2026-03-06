46d63a00bb3eb6b2583aadd7a3b5d3d49ead708a
Athif nular <athifnular@icloud.com>
2026-03-06 10:57:36 +0530

    Update frontend backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAuth } from "./auth";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://athifnular7.pythonanywhere.com",
 });
 
 api.interceptors.request.use((config) => {
