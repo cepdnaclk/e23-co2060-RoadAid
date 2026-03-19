cf908f33348b5adb52e689e9751bf20e152ed741
Chanuka Dissanayake <e23084@eng.pdn.ac.lk>
2026-03-19 13:52:16 +0530

    admin.py

--- backend/mechanics/admin.py
+++ backend/mechanics/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import MechanicProfile
 
 
-@admin.register(MechanicProfile)
+@admin.register(MechanicProfile) #Register the MechanicProfile model with the Django admin panel
 class MechanicProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "skills", "rating", "latitude", "longitude")
-    search_fields = ("user__username", "user__full_name", "skills")
-    ordering = ("-id",) 
+    list_display = ("id", "user", "skills", "rating", "latitude", "longitude") #Fields to be displayed in the admin list view
+    search_fields = ("user__username", "user__full_name", "skills") #Fields that can be searched in the admin panel search bar
+    ordering = ("-id",) # Default ordering of recording in the admin panel
