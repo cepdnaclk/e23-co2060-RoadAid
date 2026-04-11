3c0adff0dc98680ee94a8526140556e0102e26b9
Chanuka Dissanayake <e23084@eng.pdn.ac.lk>
2026-04-11 19:19:25 +0530

    urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("users.urls")),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("mechanics/", include("mechanics.urls")),
-    path("requests/", include("requests.urls")),
+    path("admin/", admin.site.urls), # Django built-in admin panel
+    path("api/", include("users.urls")), #User related end points (registration, login, profile , etc)
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"), # Accessible ate api/token/refresh
+    path("mechanics/", include("mechanics.urls")), #routed to mechanics/urls.py
+    path("requests/", include("requests.urls")), # routed to requests/urls.py
 ]
