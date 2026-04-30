a620cb733675bc75f5c64e587e38f17d4a4f831c
chanukx-v <e23084@eng.pdn.ac.lk>
2026-04-30 11:49:53 +0530

    Update the URL Patterns

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("api/", include("users.urls")), # User related end points (registration, login, profile , etc)
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"), # Accessible ate api/token/refresh
     path("mechanics/", include("mechanics.urls")), # routed to mechanics/urls.py
-    path("requests/", include("requests.urls")), # routed to requests/urls.py
+    path("requests/", include("requests.urls")), #routed to requests/urls.py
 ]
